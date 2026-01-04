2dd81a7734b3151ef490cbf25445d39b3b875935
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-01-04 21:25:50 +0100

    wip

--- donna/donna/domain/types.py
+++ donna/donna/domain/types.py
@@ -1,17 +1,27 @@
 import uuid
 from typing import NewType
 
-StoryId = NewType("StoryId", str)
+# Internal base types
+
 CounterId = NewType("CounterId", str)
 InternalId = NewType("InternalId", str)
+
+# Ids that are integer-based with a prefix and CRC
 
 WorkUnitId = NewType("WorkUnitId", InternalId)
 ActionRequestId = NewType("ActionRequestId", InternalId)
 TaskId = NewType("TaskId", InternalId)
 
+# Ids that a pure string type
+
+StoryId = NewType("StoryId", str)
+OperationId = NewType("OperationId", str)
+
+# Ids to refactor
+
 RecordId = NewType("RecordId", str)
 EventId = NewType("EventId", str)
-OperationId = NewType("OperationId", str)
+
 RecordKindId = NewType("RecordKindId", str)
 OperationResultId = NewType("OperationResultId", str)
 RecordIdTemplate = NewType("RecordIdTemplate", str)
