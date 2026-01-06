90a669b9bd82ab01c9b2bd60b21864638f622cae
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-01-06 19:44:40 +0100

    wip: events broken

--- donna/donna/machine/events.py
+++ donna/donna/machine/events.py
@@ -6,40 +6,50 @@ from donna.core.entities import BaseEntity
 from donna.domain.types import EventId, OperationId, TaskId
 
 
+# TODO: in the future we may want a strict control on event parameter types
+#       => we may want to add EventScema entity to the register
+#          and validate against it at event & event template creation time
+#       however for now we keep it simple, because:
+#       - it is easy & faster to implement
+#       - there is no end vision yet on how events should work
+
+
+EventAgrument = int | None | str
+
+
 class Event(BaseEntity):
     id: EventId
-    operation_id: OperationId
-    task_id: TaskId
+    arguments: dict[str, str]
 
     @classmethod
-    def build(cls, id: EventId, operation_id: OperationId, task_id: TaskId) -> "Event":
+    def build(cls, id: EventId, **kwargs: EventAgrument) -> "Event":
         return Event(
             id=id,
-            operation_id=operation_id,
-            task_id=task_id,
+            arguments=kwargs,
         )
 
 
 class EventTemplate(BaseEntity):
-    id: EventId | None
-    operation_id: OperationId | None
+    id: EventId
+    # TODO: we may want to add variables here to capture dynamic parts of the event id
+    arguments: dict[str, str]
+
+    @classmethod
+    def build(cls, id: EventId, **kwargs: EventAgrument) -> "EventTemplate":
+        return EventTemplate(
+            id=id,
+            arguments=kwargs,
+        )
 
     def match(self, event: Event) -> bool:
-        if self.id is not None and self.id != event.id:
+        if self.id != event.id:
             return False
 
-        if self.operation_id is not None and self.operation_id != event.operation_id:
-            return False
+        for key, value in self.arguments.items():
+            if key not in event.arguments:
+                return False
+
+            if event.arguments[key] != value:
+                return False
 
         return True
-
-    @pydantic.model_validator(mode="before")
-    @classmethod
-    def _coerce_from_scalar(cls, value: Any) -> Any:
-        if isinstance(value, cls):
-            return value
-
-        if isinstance(value, str):
-            return {"id": value, "operation_id": None}
-
-        return value
