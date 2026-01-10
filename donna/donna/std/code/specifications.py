21ab1ee83e4864f17e9830a5bff576e081dcd10e
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-01-10 16:37:47 +0100

    wip

--- donna/donna/std/code/specifications.py
+++ donna/donna/std/code/specifications.py
@@ -1,7 +1,14 @@
-from donna.domain.ids import NamespaceId
+from typing import Any
+
+import jinja2
+from jinja2.runtime import Context
+
+from donna.domain.ids import NamespaceId, FullArtifactId
 from donna.machine.artifacts import Artifact, ArtifactInfo, ArtifactKind
 from donna.machine.cells import Cell
 from donna.world.markdown import ArtifactSource
+from donna.machine.templates import RendererKind
+from donna.world.templates import RenderMode
 
 
 class Specification(Artifact):
@@ -34,4 +41,40 @@ specification_kind = SpecificationKind(
     id="specification",
     namespace_id=NamespaceId("specifications"),
     description="A specification that define various aspects of the current project.",
+)
+
+
+class Spec(RendererKind):
+
+    @jinja2.pass_context
+    def __call__(self, context: Context, *argv: Any, **kwargs: Any) -> Any:
+        render_mode: RenderMode = context["render_mode"]
+
+        artifact_id = context["artifact_id"]
+
+        if argv is None or len(argv) != 1:
+            raise ValueError("Spec renderer requires exactly one argument: specificatin_id")
+
+        match render_mode:
+            case RenderMode.cli:
+                return self.render_cli(context, artifact_id)
+
+            case RenderMode.analysis:
+                return self.render_analyze(context, artifact_id)
+
+            case _:
+                raise NotImplementedError(f"Render mode {render_mode} not implemented in Spec renderer.")
+
+    def render_cli(self, context: Context, specification_id: FullArtifactId) -> str:
+        return f"donna artifacts get specifications '{specification_id}'"
+
+    def render_analyze(self, context: Context, specification_id: FullArtifactId) -> str:
+        return f"$$donna {self.id} {specification_id} donna$$"
+
+
+spec_renderer = Spec(
+    id="spec",
+    name="Specification reference",
+    description="Instructs the agent how to read a specification.",
+    example="{{ spec('<specification_id>') }}",
 )
