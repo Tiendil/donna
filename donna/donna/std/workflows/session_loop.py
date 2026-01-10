b33eed16f053c69dff54ee26820349c3755b86a9
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-01-10 15:36:46 +0100

    wip

---
+++ donna/donna/std/workflows/session_loop.py
@@ -0,0 +1,12 @@
+# Grooming Workflow
+
+```toml donna
+start_operation_id = "run_autoflake"
+description = """
+Initiate operations to groom and refine the donna codebase: running & fixing tests, formatting code, fixing type annotations, etc.
+"""
+```
+
+Initiate operations to groom and refine the donna codebase: running & fixing tests, formatting code, fixing type annotations, etc.
+
+## Run Autoflake
