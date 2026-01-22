38d32e314f6ad66813f06cf5274a786ea9e302c4
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-01-22 19:43:23 +0100

    wip

--- donna/donna/primitives/artifacts/__init__.py
+++ donna/donna/primitives/artifacts/__init__.py
@@ -1,6 +1,5 @@
 from donna.primitives.artifacts.python import PythonArtifact, PythonModuleSectionConfig, PythonModuleSectionKind
-from donna.primitives.artifacts.specification import SpecificationKind
-from donna.primitives.artifacts.text_section import ArtifactSectionTextKind, TextConfig
+from donna.primitives.artifacts.specification import ArtifactSectionTextKind, SpecificationKind, TextConfig
 from donna.primitives.artifacts.workflow import WorkflowKind, find_not_reachable_operations
 
 __all__ = [
--- donna/donna/primitives/artifacts/specification.py
+++ donna/donna/primitives/artifacts/specification.py
@@ -1,10 +1,53 @@
+import uuid
+from typing import TYPE_CHECKING, Iterable
+
+from donna.domain.ids import FullArtifactId
 from donna.machine.artifacts import (
     Artifact,
     ArtifactConfig,
     ArtifactContent,
     ArtifactKind,
     ArtifactMeta,
+    ArtifactSection,
+    ArtifactSectionConfig,
+    ArtifactSectionKind,
+    ArtifactSectionMeta,
+    SectionContent,
 )
+
+if TYPE_CHECKING:
+    from donna.machine.changes import Change
+    from donna.machine.tasks import Task, WorkUnit
+
+
+class TextConfig(ArtifactSectionConfig):
+    pass
+
+
+class ArtifactSectionTextKind(ArtifactSectionKind):
+    def execute_section(self, task: "Task", unit: "WorkUnit", operation: ArtifactSection) -> Iterable["Change"]:
+        raise NotImplementedError("Text sections cannot be executed.")
+
+    def construct_section(self, artifact_id: FullArtifactId, section: SectionContent) -> ArtifactSection:
+        data = dict(section.config)
+
+        if "id" not in data:
+            # TODO: we should replace this hack with a proper ID generator
+            #       to keep that id stable between runs
+            #       options:
+            #       - a hash of the content
+            #       - a sequential ID generator per artifact
+            data["id"] = "text" + uuid.uuid4().hex.replace("-", "")
+
+        config = TextConfig.parse_obj(data)
+
+        return ArtifactSection(
+            id=artifact_id.to_full_local(config.id),
+            kind=config.kind,
+            title=section.title,
+            description=section.description,
+            meta=ArtifactSectionMeta(),
+        )
 
 
 class SpecificationKind(ArtifactKind):
