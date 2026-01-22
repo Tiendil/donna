f9905f07cbf746bce50c7f7c84614ac269a78ae5
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-01-22 19:27:40 +0100

    wip

--- donna/donna/primitives/artifacts/__init__.py
+++ donna/donna/primitives/artifacts/__init__.py
@@ -1,5 +1,4 @@
-from donna.primitives.artifacts.python_artifact import PythonArtifact
-from donna.primitives.artifacts.python_module import PythonModuleSectionConfig, PythonModuleSectionKind
+from donna.primitives.artifacts.python import PythonArtifact, PythonModuleSectionConfig, PythonModuleSectionKind
 from donna.primitives.artifacts.specification import SpecificationKind
 from donna.primitives.artifacts.text_section import ArtifactSectionTextKind, TextConfig
 from donna.primitives.artifacts.workflow import WorkflowKind, find_not_reachable_operations
