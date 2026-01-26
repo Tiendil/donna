0c6ee839bafb06f6da5655b1e854187afaa70d58
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-01-26 22:42:36 +0100

    wip

--- donna/donna/cli/utils.py
+++ donna/donna/cli/utils.py
@@ -1,10 +1,13 @@
+import sys
 from collections.abc import Iterable
 
-import typer
-
 from donna.protocol.cells import Cell
+from donna.protocol.modes import get_cell_formatter
 
 
 def output_cells(cells: Iterable[Cell]) -> None:
-    for cell in cells:
-        typer.echo(cell.render())
+    formatter = get_cell_formatter()
+
+    output = formatter.format_cells(list(cells))
+
+    sys.stdout.buffer.write(output)
