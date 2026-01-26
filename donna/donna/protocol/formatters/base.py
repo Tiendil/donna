22e7a881a9babf42601f1db947618a31c020d464
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-01-26 22:45:07 +0100

    wip

--- donna/donna/protocol/formatters/base.py
+++ donna/donna/protocol/formatters/base.py
@@ -5,3 +5,6 @@ class Formatter:
 
     def format_cell(self, cell: Cell) -> bytes:
         raise NotImplementedError()
+
+    def format_cells(self, cells: list[Cell]) -> bytes:
+        return b"\n".join([self.format_cell(cell) for cell in cells])
