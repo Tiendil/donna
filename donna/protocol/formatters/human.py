9a7722a9ef40dcbc2b37587ba5a7cce9013c5e77
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-02-14 21:21:21 +0100

    wip

--- donna/protocol/formatters/human.py
+++ donna/protocol/formatters/human.py
@@ -31,9 +31,8 @@ class Formatter(BaseFormatter):
         timestamp = record.timestamp.time().isoformat("seconds")
         actor_id = record.actor_id or "-"
         current_task_id = record.current_task_id.short if record.current_task_id is not None else "-"
-        current_operation_id = record.current_operation_id.short if record.current_operation_id is not None else "-"
         output = (
-            f"{timestamp} " f"[{actor_id}] " f"[{current_task_id}] " f"[{current_operation_id}] " f"{record.message}"
+            f"{timestamp} [{actor_id}] [{current_task_id}] {record.message}"
         )
         return output.encode()
 
