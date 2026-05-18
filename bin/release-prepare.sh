671b0f64e163744ebba7356e974e3513b2f20f45
Aliaksei Yaletski (Tiendil) <a.eletsky@gmail.com>
2026-05-18 22:36:52 +0200

    fixed prepare-release.sh

--- bin/release-prepare.sh
+++ bin/release-prepare.sh
@@ -1,42 +1,47 @@
 #!/usr/bin/bash
 
-set -e
+set -euo pipefail
 
-export BUMP_VERSION=$1
+BUMP_VERSION=${1:-}
+
+if [[ "$BUMP_VERSION" != "minor" && "$BUMP_VERSION" != "patch" ]]; then
+    echo "usage: ./bin/release-prepare.sh minor|patch"
+    exit 1
+fi
 
 echo "Bumping version as $BUMP_VERSION"
 
-export NEXT_VERSION=$(uv version --bump $BUMP_VERSION --short)
-export NEXT_VERSION_TAG="release-$NEXT_VERSION"
-
-echo "Install dependencies"
-
-uv sync
+NEXT_VERSION=$(./bin/dev.sh uv version --bump "$BUMP_VERSION" --short)
+NEXT_VERSION_TAG="release-$NEXT_VERSION"
 
 echo "Update change log"
 
-uv run changy version create $NEXT_VERSION
+run_changy() {
+    ./bin/dev.sh env UV_NO_SYNC=0 uv run --dev changy "$@"
+}
+
+run_changy version create "$NEXT_VERSION"
 
 echo "Generate changelog"
 
-uv run changy changelog create
+run_changy changelog create
 
-export COMMIT_BODY=$(uv run changy version show $NEXT_VERSION)
+COMMIT_BODY=$(run_changy version show "$NEXT_VERSION")
 
 echo "New version is $NEXT_VERSION"
 echo "New version tag $NEXT_VERSION_TAG"
 
 echo "Building Python package"
 
-uv build
+./bin/dev.sh uv build
 
 echo "Commit changes"
 
-git add -A
+git add pyproject.toml uv.lock CHANGELOG.md changes
 git commit -m "Release $NEXT_VERSION" -m "$COMMIT_BODY"
 git push
 
 echo "Create tag"
 
-git tag $NEXT_VERSION_TAG
-git push origin $NEXT_VERSION_TAG
+git tag "$NEXT_VERSION_TAG"
+git push origin "$NEXT_VERSION_TAG"
